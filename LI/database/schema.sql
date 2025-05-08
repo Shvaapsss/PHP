713ca56342fda33d210c585418cadc4468878fef
Shvaapsss <145658659+Shvaapsss@users.noreply.github.com>
2025-05-08 09:58:08 +0300

    Update schema.sql

--- LI/database/schema.sql
+++ LI/database/schema.sql
@@ -1,16 +1,14 @@
+-- users таблица
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE,
-    password VARCHAR(255),
-    role ENUM('user', 'admin') DEFAULT 'user'
+    login VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'user'
 );
 
+-- resources таблица
 CREATE TABLE resources (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    description TEXT,
-    category VARCHAR(100),
-    is_active BOOLEAN,
-    created_by INT,
-    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE
+    title VARCHAR(255) NOT NULL,
+    description TEXT
 );
