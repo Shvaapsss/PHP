a8a778ada17936ba73bb75ef3989be15330caa94
Shvaapsss <145658659+Shvaapsss@users.noreply.github.com>
2025-05-08 09:49:07 +0300

    Delete public directory

--- public/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../includes/auth.php';
-?>
-<h1>Личный кабинет</h1>
-<p>Вы вошли как пользователь.</p>
-<a href="create_resource.php">Создать ресурс</a> |
-<a href="search.php">Поиск</a> |
-<a href="logout.php">Выход</a>
-<?php if ($_SESSION['role'] === 'admin') echo '<p><a href="admin_panel.php">Админка</a></p>'; ?>
