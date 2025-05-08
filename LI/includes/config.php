5ebb7974ab325a357c81bad49df8452325bf5546
Shvaapsss <145658659+Shvaapsss@users.noreply.github.com>
2025-05-08 10:03:42 +0300

    Update config.php

--- LI/includes/config.php
+++ LI/includes/config.php
@@ -1,3 +1,15 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "appdb");
-if ($conn->connect_error) die("Ошибка подключения");
+// Настройки для подключения к базе данных
+$servername = "localhost";
+$username = "root"; // Убедитесь, что это ваши данные
+$password = "";     // Ваш пароль для подключения
+$dbname = "web_app"; // Имя вашей базы данных
+
+// Создание соединения с базой данных
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка на успешность подключения
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+}
+?>
